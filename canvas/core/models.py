77d7e23559475beff7a8ecffde0fcdd0c368d0fc
Kashish <immi.kashish@gmail.com>
2024-07-11 10:56:34 +0530

    Update admin interface

--- canvas/core/models.py
+++ canvas/core/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Stand(models.Model):
     title = models.TextField(default=None,unique=True)
-    height = models.IntegerField(default=None)
-    width = models.IntegerField(default=None)
+    height = models.IntegerField(default=None,help_text="Height in pixels")
+    width = models.IntegerField(default=None,help_text="Width in pixels")
 
     def __str__(self):
         return self.title
